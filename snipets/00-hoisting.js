d1ab9c367037a5e25e639c08340ef2becb241203
manufosela <manufosela@gmail.com>
2023-04-19 16:17:32 +0200

    2 files modified

--- snipets/00-hoisting.js
+++ snipets/00-hoisting.js
@@ -3,4 +3,12 @@ var a;
 console.log(a);
 a = 1;
 console.log(a);
+// */
 
+/*
+console.log(b);
+let b;
+console.log(b);
+b = 2;
+console.log(b);
+// */
--- snipets/01-setTimeout ES5.js
+++ snipets/01-setTimeout ES5.js
@@ -1,3 +1,9 @@
-// Copyright 2023 manufosela.
-// SPDX-License-Identifier: GPL-3.0-only
-
+/** Clase con prototype ES5 */
+function Saludo(nombre) {
+  this.nombre = nombre;
+}
+Saludo.prototype.hola = function () {
+  return 'Hola, ' + this.nombre;
+};
+const saludo = new Saludo('pepe');
+console.log(saludo.hola());
